48a69e1da2820a4605b9ea4e681bf694e3fda4c0
Stefan Roeck <stefan.roeck@gmail.com>
2022-11-18 21:47:42 +0100

    added test for log file with simple timestamps

--- src/analyzer/integration.test.ts
+++ src/analyzer/integration.test.ts
@@ -37,6 +37,20 @@ describe("parser and analyzer", () => {
 
     })
 
+    it("log with simple timestamps", () => {
+        const content = readFileSync(__dirname + "/testfiles/logWithSimpleTimestamps.log", "utf8");
+
+        const result = analyze(parse(content));
+
+        expect(result.mavenPlugins.length).toEqual(65);
+        expect(result.mavenPlugins.map(r => r.thread).filter((t, idx, arr) => arr.indexOf(t) === idx)).toEqual(["main"]);
+        expect(durationSumForPlugin(result.mavenPlugins, "maven-compiler-plugin")).toEqual(25000);
+        expect(durationSumForPlugin(result.mavenPlugins, "maven-javadoc-plugin")).toEqual(71000);
+
+        expect(result.modules).toHaveLength(1);
+        expect(result.modules).toEqual([{ "compiledSources": 478, "compiledTestSources": 450, "copiedResources": 0, "copiedTestResources": 0, "module": "commons-lang3" }]);
+    })
+
     function durationSumForPlugin(result: AnalyzerRow[], plugin: string): number {
         return result.filter(r => r.plugin === plugin).reduce((prev, curr) => prev + curr.duration, 0);
     }
