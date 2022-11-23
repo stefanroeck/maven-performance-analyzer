42db48d701908c166fcd1374e7888ede0d99d0e4
Stefan Roeck <stefan.roeck@gmail.com>
2022-11-23 22:30:59 +0100

    display version number

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,11 +3,14 @@ import Box from "@mui/material/Box";
 
 
 export const Header = () => {
+    const version = `v${process.env.REACT_APP_VERSION || ""}`;
+
     return <Box sx={{ color: "#111", borderBottom: "1px solid gray", padding: "0 20px", whiteSpace: "nowrap" }}>
         <img alt="Logo" src="./logo.png" height="120px" />
         <Box sx={{ display: "inline-block", verticalAlign: "top", marginLeft: "20px" }}>
             <Typography variant="h2" >Maven Performance Analyzer</Typography>
             <Typography variant={"subtitle1"}>Analyze Maven Logs to identify bottlenecks and speed up your build.</Typography>
+            <Typography variant={"subtitle2"}>{version}</Typography>
             <a href="https://github.com/stefanroeck/maven-performance-analyzer">
                 <img alt="Visit me at Github" src="GitHub-Mark-64px.png" style={{
                     height: "40px",
